7b8375e6bd3afd8bdebe17dc6e356d4a977c8251
Kevin Bullaughey <kbullaughey@gmail.com>
2011-11-17 23:13:42 -0600

    scripts to run my burnin and first balancing selection analysis

--- runs/balancing_selection/plot.r
+++ runs/balancing_selection/plot.r
@@ -1,13 +1,13 @@
 library(grid)
 source("../../src/r/evolveq.R")
 
-pip <- pipe("find out -name 'shift_opt_0_to_1-times_100_1900-s_0.1.*.out'", "r")
+pip <- pipe("find out -name 'shift_opt_0_to_1-times_100_10900-s_0.1.*.out'", "r")
 files <- scan(pip, what="", sep="\n")
 close(pip)
 
 runs <- lapply(files, load.simoutput)
 
-pdf(file="balancing_selection_runs.pdf", height=3.5, width=6)
+pdf(file="balancing_selection_runs-long.pdf", height=3.5, width=6)
 trash <- lapply(runs, function(run) {
   grid.newpage()
   plot.freqs(run, pdf.file=NULL, color.by="effects")
