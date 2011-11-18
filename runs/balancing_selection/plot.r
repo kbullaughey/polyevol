92b429fb5f680b46facd676a5d898bfbdd545fff
Kevin Bullaughey <kbullaughey@gmail.com>
2011-11-18 12:33:46 -0600

    changed PDF name

--- runs/balancing_selection/plot.r
+++ runs/balancing_selection/plot.r
@@ -7,7 +7,7 @@ close(pip)
 
 runs <- lapply(files, load.simoutput)
 
-pdf(file="balancing_selection_runs.pdf", height=3.5, width=6)
+pdf(file="balancing_selection_runs-set2.pdf", height=3.5, width=6)
 trash <- lapply(runs, function(run) {
   grid.newpage()
   plot.freqs(run, pdf.file=NULL, color.by="effects")
