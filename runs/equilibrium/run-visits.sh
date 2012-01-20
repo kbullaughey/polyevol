b20ed40aa86815990349b1a814becb7345f4305f
Kevin Bullaughey <kbullaughey@gmail.com>
2012-01-19 20:35:17 -0600

    run script for tabulating visits to each derived-allele count

---
+++ runs/equilibrium/run-visits.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#$ -cwd
+#$ -l h_vmem=1g
+#$ -o grid/equilibrium.$JOB_ID.out
+#$ -e grid/equilibrium.$JOB_ID.err
+
+if [ "x" == "x$1" ] ; then
+  echo "Must specify params" >&2
+  exit 1
+fi
+params=$*
+analysis="visits"
+
+seed=`$HOME/bin/rand.pl`
+file_suffix=`echo "$params" | tr '= ' '_-' | sed 's/--/-/g' | sed 's/^-//'`
+basedir="out/$analysis/$file_suffix"
+outfile="$basedir/$analysis-$file_suffix.$JOB_ID.out.gz"
+
+if [ ! -d $basedir ] ; then
+  mkdir -p $basedir
+fi
+
+$HOME/bin/quant -m infinite --freqs=even -s 0.1 -N 1000 --opts=0 --loci=0 --burnin=10000 \
+    --times=1000000 --disable-all-stats --enable-stat=visits $params \
+  | gzip > $outfile
+
+# END
