4d0df738e0828e287853f727f3b96f62472cb86a
Kevin Bullaughey <kbullaughey@gmail.com>
2012-01-20 10:48:47 +0800

    scripts for looking at example runs with various mutation rates

---
+++ runs/equilibrium/launch-visits-examples.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+./run-visits-examples.sh -u 1e-03 --effects=1
+./run-visits-examples.sh -u 4e-03 --effects=1
+./run-visits-examples.sh -u 1.6e-02 --effects=1
+./run-visits-examples.sh -u 6.4e-02 --effects=1
+./run-visits-examples.sh -u 3.2e-02 --effects=1
+./run-visits-examples.sh -u 8e-03 --effects=1
+./run-visits-examples.sh -u 1.28e-01 --effects=1
+./run-visits-examples.sh -u 2.56e-01 --effects=1
+./run-visits-examples.sh -u 5.0e-02 --effects=1
