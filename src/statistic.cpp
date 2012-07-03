72bdf0ab2b8a96547b5ccd97f4a2087a47e695cd
Kevin Bullaughey <kbullaughey@gmail.com>
2012-07-03 12:00:07 -0400

    added first_moment to registered statistics

--- src/statistic.cpp
+++ src/statistic.cpp
@@ -20,6 +20,7 @@ void Statistic::initialize_defaults(void) {
   directory[string("visits")] = false;
   directory[string("fixations")] = false;
   directory[string("segsites")] = false;
+  directory[string("first_moment")] = false;
 }
 
 /* turn off all the statistics */
